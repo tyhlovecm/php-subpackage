14081754467bc7c5498d42af9fc4c2c36e5dd468
tongyh <18572821456@163.com>
2020-01-15 12:04:07 +0800

    1

--- test/testApkparse.php
+++ test/testApkparse.php
@@ -1,1 +1,18 @@
 <?php
+// 识别APK信息
+require "../vendor/autoload.php";
+
+$apk = new \ApkParser\Parser($this->targetFile);
+$manifest = $apk->getManifest();
+
+// 包名
+$package_name = $manifest->getPackageName();
+
+// 版本号
+$version_name = $manifest->getVersionName();
+
+// 版本编号
+$version_code = $manifest->getVersionCode();
+
+// 其他方法参考官方文档
+var_dump($package_name, $version_name, $version_code);
